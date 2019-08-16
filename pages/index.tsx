d09a7844623b8e3d8730dd7ba78b9abf17182c54
otaku.denka <otaku.denka@gmail.com>
2019-08-17 07:56:02 +0800

    feat(next.config.js .babelrc): use ant design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
-import { NextPage } from 'next'
+import * as React from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import { NextPage } from 'next';
+import { Button } from 'antd';
 
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>Hello Next.js 👋</h1>
+      <Button>asadasdad</Button>
       <p>
         <Link href="/about">
           <a>About</a>
         </Link>
       </p>
     </Layout>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
